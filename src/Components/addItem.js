832cb188d5a4a445665aa9f00446fb5e54958f24
Odosa <izodosaotabor@gmail.com>
2023-03-22 21:44:11 +0100

    update

--- src/Components/addItem.js
+++ src/Components/addItem.js
@@ -7,7 +7,7 @@ const AddItem = ({ onAdd }) => {
     const [message, setMessage] = useState("")
     const [messageType, setMessageType] = useState("")
     const [item, setItem] = useState('')
-    const [inStock, setInStock] = useState(false)
+    const [inStock, setInStock] = useState(true)
     const [amount, setAmount] = useState(1)
     const [damaged, setDamaged] = useState('False')
     const [date, setDate] = useState('')
@@ -47,37 +47,38 @@ const AddItem = ({ onAdd }) => {
 return (
     <>
         {showAlert && <Alert type={messageType} message={message} close={() => setShowAlert(!showAlert)} />}
-        <form className="add-form" onSubmit={onSubmit}>        
-            <div className="form-control">
-                <label htmlFor="">ITEM</label>
-                <input type="text" placeholder="Add Item" value={item} onChange={(e) => setItem(e.target.value)} />
-            </div>
-            <div className="form-control">
-                <label htmlFor="">Amount</label>
-                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
-            </div>
-            <div className="form-control-check">
-                <label htmlFor="">In Stock
-                    <input type='checkbox' checked={inStock} value={inStock} onChange={(e) => setInStock(e.currentTarget.checked)}/>
-                </label>
-                <label htmlFor="damage">Damaged:</label>
-                <select name="damage" value={damaged} onChange={(e) => setDamaged(e.target.value)}>
-                    <option value="False">False</option>
-                    <option value="True">True</option>
-                    <option value="unknown">Unknown</option>
-                </select>
-            </div>
-            <div className="form-control-check">
-                <input type="date" onChange={(event) => setDate(event.target.value)}/>
-            </div>
+        <div className="add-form">
+            <form className="form" onSubmit={onSubmit}>
+                <h1>ADD ITEM</h1>
+                <div className="form-control">
+                    <label htmlFor="">ITEM</label>
+                    <input type="text" placeholder="Add Item" value={item} onChange={(e) => setItem(e.target.value)} />
+                    <label htmlFor="">Amount</label>
+                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+                </div>
+                <div className="form-control-check">
+                    <label htmlFor="">In Stock
+                        <input type='checkbox' checked={inStock} value={inStock} onChange={(e) => setInStock(e.currentTarget.checked)}/>
+                    </label>
+                    <label htmlFor="damage">Damaged:</label>
+                    <select name="damage" value={damaged} onChange={(e) => setDamaged(e.target.value)}>
+                        <option value="False">False</option>
+                        <option value="True">True</option>
+                        <option value="unknown">Unknown</option>
+                    </select>
+                </div>
+                <div className="form-control-check">
+                    <input type="date" onChange={(event) => setDate(event.target.value)}/>
+                </div>
 
-            <div className="form-control">
-                <label htmlFor="">Comment</label>
-                <input type="text" placeholder="Returned by person's name/ given to person's name" value={comment} onChange={(e) => setComment(e.target.value)} />
-            </div>
+                <div className="form-control">
+                    <label htmlFor="">Comment</label>
+                    <input type="text" placeholder="Returned by person's name/ given to person's name" value={comment} onChange={(e) => setComment(e.target.value)} />
+                </div>
 
-            <input type="submit" value="save item" className="btn" />
-        </form>
+                <input type="submit" value="save item" className="btn" />
+            </form>
+        </div>
     </>
 )
 }
--- src/Components/editItem.js
+++ src/Components/editItem.js
@@ -40,36 +40,39 @@ const EditItem = ({onSaveEdit, onEditClick, onCancel}) => {
 return (
     <>
         {showAlert && <Alert type={messageType} message={message} close={() => setShowAlert(!showAlert)} />}
-        <form className="add-form edit-form" onSubmit={onSubmit}>
-            <h1>EDIT Item</h1>
-            <h2>{onEditClick.item}</h2>
-            <div className="form-control">
-                    <label htmlFor="">Amount</label>
-                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        <div className="add-form">
+            <form className="form" onSubmit={onSubmit}>
+                <h1>EDIT ITEM</h1>
+                <br />
+                <h2>{onEditClick.item}</h2>
+                <div className="form-control">
+                        <label htmlFor="">Amount</label>
+                        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+                    </div>
+                <div className="form-control">
+                    <label htmlFor="">In Stock
+                        <input type='checkbox' checked={inStock} onChange={(e) => setInStock(e.currentTarget.checked)}/>
+                    </label>
+                    <label htmlFor="damage">Damaged:</label>
+                    <select name="damage" value={damaged} onChange={(e) => setDamaged(e.target.value)}>
+                        <option value="False">False</option>
+                        <option value="True">True</option>
+                        <option value="unknown">Unknown</option>
+                    </select>
                 </div>
-            <div className="form-control-check">
-                <label htmlFor="">In Stock
-                    <input type='checkbox' checked={inStock} onChange={(e) => setInStock(e.currentTarget.checked)}/>
-                </label>
-                <label htmlFor="damage">Damaged:</label>
-                <select name="damage" value={damaged} onChange={(e) => setDamaged(e.target.value)}>
-                    <option value="False">False</option>
-                    <option value="True">True</option>
-                    <option value="unknown">Unknown</option>
-                </select>
-            </div>
-            <div className="form-control-check">
-                <input type="date" value={date} onChange={(event) => setDate(event.target.value)}/>
-            </div>
+                <div className="form-control-check">
+                    <input type="date" value={date} onChange={(event) => setDate(event.target.value)}/>
+                </div>
 
-            <div className="form-control">
-                <label htmlFor="">Comment</label>
-                <input type="text" placeholder="Returned by person's name/ given to person's name" value={comment} onChange={(e) => setComment(e.target.value)} />
-            </div>
+                <div className="form-control">
+                    <label htmlFor="">Comment</label>
+                    <input type="text" placeholder="Returned by person's name/ given to person's name" value={comment} onChange={(e) => setComment(e.target.value)} />
+                </div>
 
-            <input type="submit" value="save" className="btns" />
-            <input type="button" onClick={onCancel} value="cancel" className="btns cancelbtn" />
-        </form>
+                <input type="submit" value="save" className="btn" />
+                <input type="button" onClick={onCancel} value="cancel" className="btn cancelBtn" />
+            </form>
+        </div>
     </>
 )
 }
